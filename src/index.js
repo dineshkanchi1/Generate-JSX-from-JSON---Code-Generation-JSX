a35dd7c5a0c0facf3dc180daf37ccaff5b8ae608
k.d.v.kumar123@gmail.com <k.d.v.kumar123@gmail.com>
2020-10-01 17:03:46 +0530

    commited

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
- function toTitleCase(str){
+function toTitleCase(str){
     return str.replace(
         /\w\S+/g,
         function(txt){
             return txt.charAt(0).toUpperCase()+txt.substr(1).toLowerCase();
         }
     ).replace(" ","");
- }
- function toCamelCase(str){
-     str=str.replace("-"," ")
-     return str.replace(/\s(.)/g, function($1){return $1.toUpperCase();}).replace(/\s/g,'').replace(/^(.)/,function($1){return $1.toLowerCase();});
- }
-function generateComponent(obj){
-    let tag = "<"
-    tag += toTitleCase(obj.name)+" "
+}
+function toCamelCase(str){
+    str=str.replace("-"," ")
+    return str.replace(/\s(.)/g, function($1){return $1.toUpperCase();}).replace(/\s/g,'').replace(/^(.)/,function($1){return $1.toLowerCase();});
+}
+function recur(obj){
+    let retStr = "<"
+    retStr += toTitleCase(obj.name)+" "
     if(obj.style!==undefined && Object.keys(obj.style).length>0){
-        let styles = Object.keys(obj.style)
-        tag += "style={{"
-        for(let i=0;i<styles.length;i++){
-            tag+=toCamelCase(styles[i])+':"'+obj.style[styles[i]]+'",'
+        let stypeKeys = Object.keys(obj.style)
+        retStr += "style={{"
+        for(let i=0;i<stypeKeys.length;i++){
+            retStr+=toCamelCase(stypeKeys[i])+':"'+obj.style[stypeKeys[i]]+'",'
         }
-        tag=tag+"}}"
+        retStr=retStr.substr(0,retStr.length-1)+"}}"
     }
     if(obj.children !== undefined && obj.children.length>0){
-        tag+=">\n";
+        retStr+=">\n";
         for(let i=0;i<obj.children.length;i++){
-            tag+=generateComponent(obj.children[i])
+            retStr+=recur(obj.children[i])
         }
-        tag+="</"+toTitleCase(obj.name)+">"
+        retStr+="</"+toTitleCase(obj.name)+">"
     }else{
-        tag+="/>"
+        retStr+="/>"
     }
-    return tag
+    return retStr
 }
 function generateCodeFromObject(obj){
-    return generateComponent(obj)
-}
+        return recur(obj)
+   }
    
 module.exports=generateCodeFromObject;
 
