f7ee44e4406db9c4d2cd64ae45e2a08ee70f9027
k.d.v.kumar123@gmail.com <k.d.v.kumar123@gmail.com>
2020-10-01 16:45:07 +0530

    commited

--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
  function toTitleCase(str){
-//     return str.replace(
-//         /\w\S+/g,
-//         function(txt){
-//             return txt.charAt(0).toUpperCase()+txt.substr(1).toLowerCase();
-//         }
-//     ).replace(" ","");
-        return str.charAt(0).toUpperCase()+str.substr(1).toLowerCase();
+    return str.replace(
+        /\w\S+/g,
+        function(txt){
+            return txt.charAt(0).toUpperCase()+txt.substr(1).toLowerCase();
+        }
+    ).replace(" ","");
  }
  function toCamelCase(str){
-//     str=str.replace("-"," ")
-//     return str.replace(/\s(.)/g, function($1){return $1.toUpperCase();}).replace(/\s/g,'').replace(/^(.)/,function($1){return $1.toLowerCase();});
-        return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+     str=str.replace("-"," ")
+     return str.replace(/\s(.)/g, function($1){return $1.toUpperCase();}).replace(/\s/g,'').replace(/^(.)/,function($1){return $1.toLowerCase();});
  }
 function recur(obj){
     let tag = "<"
