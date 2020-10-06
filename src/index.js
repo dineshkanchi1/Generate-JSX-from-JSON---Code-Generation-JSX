8c4f6dbf909315634b2eb7ef885d510944f5f6c6
k.d.v.kumar123@gmail.com <k.d.v.kumar123@gmail.com>
2020-10-06 13:58:38 +0530

    commied

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 function toTitleCase(str){
-    return str.replace(
-        /\w\S+/g,
-        function(txt){
-            return txt.charAt(0).toUpperCase()+txt.substr(1).toLowerCase();
-        }
-    ).replace(" ","");
+
+    let s=str.split(" ");
+    let res="";
+    for(let i in s){
+        res+=i[0].toUpperCase()+i.substr(1).toLowerCase();
+    }
+    return res;
+
 }
 function toCamelCase(str){
     return str.replace("-"," ").replace(/\s(.)/g, function($1){return $1.toUpperCase();}).replace(/\s/g,'').replace(/^(.)/,function($1){return $1.toLowerCase();});
